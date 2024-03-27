7cc6b88408c0bb7b8f40567de23cb4a45c84aca5
Hamza Khan <se.hamzakhan@gmail.com>
2024-03-27 23:23:32 +0100

    Complet project

---
+++ app/lib/sanity.ts
@@ -0,0 +1,13 @@
+import { createClient } from "next-sanity";
+
+
+const projectId = process.env.PROJECT_ID
+const dataset = "production";
+const apiVersion = "2023-01-01";
+
+export const client = createClient({
+  projectId,
+  dataset,
+  apiVersion,
+  useCdn: true,
+});
